508d95327fad23793f2e25009c88de48752fe271
Ander Berruezo Aguirre <berru0429@gmail.com>
2024-03-07 12:45:40 +0100

    UAT zuzenduta

--- menu/tree_items.js
+++ menu/tree_items.js
@@ -119,7 +119,7 @@ var TREE_ITEMS = [
 					[' Proba Log-a', null],
 					[' Proba Script-a', null],
 					[' Proben emaitzak', null],
-					[' User acceptace test (UAT)','../Proiektua/MemoriarenEranskinak/User-Accetptance-Test-Plan-Free-Word-Template.pdf'],
+					[' User acceptace test (UAT)','../Proiektua/MemoriarenEranskinak/User-Accetptance-Test-Plan-Free-Word.pdf'],
 					[' RFP erantzunen hasierako ebaluazioa','../Proiektua/MemoriarenEranskinak/Software projects proposals evaluation template.pdf'],
 
 				],
